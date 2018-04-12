1c6dbe6f30e3c0cf94c185b8410787467c4b0a79
Santyid <santiago-2108@hotmail.com>
2018-04-12 14:10:03 -0500

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,25 @@ import { AngularFirestore } from 'angularfire2/firestore';
 	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
+	//Lista de los todos que se extraigan de la base de datos
 	todos$: Todo[];
+	//Lista de los todo filtrados false
 	todosFalse: Todo[];
+	//Variable countTodos que se le asiganara la cantidad de los todos
 	countTodos: number;
 
+	/**
+	 * Constructor del componente
+	 * @param todosService Se inyecta el servicio que se utilizara para la comunicacion con el service y la bd
+	 */
 	constructor(private todosService: TodoService) {
 		this.allTodos();
 	}
+
+	/**
+	*allTodos metodo que obtiene todos los elementos de la bd por medio del service y adicionalemente cuenta cuantos elementos
+	*tienen un status false y se los agrega a una variable
+	 */
 
 	allTodos() {
 		this.todosService.selectTodos().subscribe((content) => {
@@ -26,24 +38,49 @@ export class AppComponent {
 		});
 	}
 
+	/**
+	 * addWord metodo el cual recibe el nombre de la palabra y
+	 *lo envia al service para asi poder agregarla
+	 * @param input nombre de la palabra
+	 */
 	addWord(input: HTMLInputElement) {
 		this.todosService.addWords(input.value);
 		input.value = '';
 	}
 
+	/**
+	 * Metodo el cual cambia el status del elemento de la bd,
+	 * en el que se envian los parametros para cambiar el estado del elemento
+	 * @param id id del elemento todo que se quiere modificar
+	 * @param status estado del elemendo que se va a cambiar
+	 */
 	changeStatu(id: string, status: boolean) {
 		this.todosService.changeStatu(id, status);
 	}
 
+	/**
+	 * Metodo para cambiar todos los elementos de la base de datos
+	 * @param status estado en el que se quiere que todos los elementos tengan
+	 */
 	changeStatusAll(status: boolean) {
 		this.todosService.changeStatusAll(this.todos$, status);
 	}
+
+	/**
+	 *StatusTrue metodo el cual filtra los elementos de la bd por el
+	 *estado true y los agrega a la lista de los elementos
+	 */
 	statusTrue() {
 		this.todosService.selectTodos().subscribe((content) => {
 			this.todos$ = content;
 			this.todos$ = this.todos$.filter((todo) => todo.status == true);
 		});
 	}
+
+	/**
+	 * StatusFalse metodo el cual filtra los elementos de la bd por el
+	 * estado false y los agrega a la lista de elementos
+	 */
 	statusFalse() {
 		this.todosService.selectTodos().subscribe((content) => {
 			this.todos$ = content;
@@ -51,11 +88,20 @@ export class AppComponent {
 		});
 	}
 
+	/**
+	 *Metodo el cual elimina los elementos de la lista los cuales
+	 *tengan el status true  
+	 */
 	deleteStatusTrue() {
 		this.todos$ = this.todos$.filter((todo) => todo.status == true);
 		this.todosService.deleteStatusTrue(this.todos$);
 		this.allTodos();
 	}
+
+	/**
+	 * Metodo para eliminar un elemento especifico de bd
+	 * @param id id del elemento
+	 */
 	deleteTodo(id: string) {
 		this.todosService.deleteTodo(id);
 		console.log(id);
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -4,32 +4,62 @@ import { AngularFirestore } from 'angularfire2/firestore';
 
 @Injectable()
 export class TodoService {
+	/**
+	 * Constructor que inyecta la db.
+	 * @param db 
+	 */
 	constructor(private db: AngularFirestore) {}
 
+	/**
+	 * Metodo que extrae todos los elementos todos de la base de datos
+	 */
 	selectTodos() {
 		return this.db.collection<Todo>('todos').valueChanges();
 	}
 
+	/**
+	 * Metodo de registro de elemetos todos a la base de datos
+	 * @param word palabra o nombre con el cual se va a guardar el elemento
+	 */
 	addWords(word: string) {
 		const id = this.db.createId();
 		const status = false;
 		this.db.collection('todos').doc(id).set({ id, word, status });
 	}
 
+	/**
+	 * Metodo el cual cambia el estado del elemento todo en la base de datos
+	 * @param id id del elemento todo
+	 * @param status estado del elemento que se quiere cambiar
+	 */
 	changeStatu(id: string, status: boolean) {
 		this.db.collection('todos').doc(id).update({ status });
 	}
 
+	/**
+	 * Metodo el cual cambia el estado de todos los elementos todos de la bd
+	 * @param todo lista de elementos todos
+	 * @param status estado al que se quiere cambiar los elementos
+	 */
 	changeStatusAll(todo: Todo[], status: boolean) {
 		for (const todos of todo) {
 			this.db.collection('todos').doc(todos.id).update({ status });
 		}
 	}
 
+	/**
+	 * Metodo de elminacion de un elemento todo en la base de datos
+	 * @param id id del elemento que se quiere eliminar
+	 */
 	deleteTodo(id: string) {
 		this.db.collection('todos').doc(id).delete();
 	}
 
+	/**
+	 * Metodo para eliminar los elementos todos de la bd que tengan
+	 * un estado true 
+	 * @param todo lista de los elementos todos con estado true
+	 */
 	deleteStatusTrue(todo: Todo[]) {
 		for (const todos of todo) {
 			this.db.collection('todos').doc(todos.id).delete();
